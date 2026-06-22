1131248a9c43dae9034f57591683840531b3c650
André Arko <andre@arko.net>
2026-06-21 18:35:59 -0700

    update the echoes to use the 1mb bin file

--- echo-direct.js
+++ echo-direct.js
@@ -1,17 +1,17 @@
 import http from "k6/http";
 import { check } from "k6";
 
+const binFile = open("1mb.bin", "b");
+
 export const options = {
-  vus: 500,
-  iterations: 10000,
+  vus: 10,
+  iterations: 1000,
 };
 
 export default function () {
-  let res = http.post(
-    "https://httpbin.oblivious.network/anything",
-    { hello: "hi" },
-    { headers: { "Content-Type": "message/ohttp-chunked-req" } },
-  );
+  let res = http.post("https://httpbin.oblivious.network/headers", binFile, {
+    headers: { "Content-Type": "message/ohttp-chunked-req" },
+  });
   check(res, {
     "status is 200": (r) => r.status === 200,
     "protocol is HTTP/2": (r) => r.proto === "HTTP/2.0",
--- echo-relay.js
+++ echo-relay.js
@@ -4,18 +4,14 @@ import { check } from "k6";
 const binFile = open("1mb.bin", "b");
 
 export const options = {
-  vus: 500,
-  iterations: 10000,
+  vus: 10,
+  iterations: 10,
 };
 
 export default function () {
-  let res = http.post(
-    "https://relay-dev.oblivious.network/echo",
-    { hello: "hi" },
-    {
-      headers: { "Content-Type": "message/ohttp-chunked-req" },
-    },
-  );
+  let res = http.post("https://relay-dev.oblivious.network/echo", binFile, {
+    headers: { "Content-Type": "message/ohttp-chunked-req" },
+  });
   check(res, {
     "status is 200": (r) => r.status === 200,
     "protocol is HTTP/2": (r) => r.proto === "HTTP/2.0",

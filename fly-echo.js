f809019ccb60b5469c89dfae41fc8a47b9385030
André Arko <andre@arko.net>
2026-08-16 01:46:08 -0700

    push the fly relay harder

--- fly-echo.js
+++ fly-echo.js
@@ -1,19 +1,22 @@
 import http from "k6/http";
 import { check } from "k6";
+import { randomString } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 export const options = {
-  vus: 16,
+  vus: 1000,
   duration: "20s",
 };
 
+const body = randomString(1024);
+
 export default function () {
-  let res = http.post("https://oblivious-relay-lax.fly.dev/echo", "hello", {
+  let res = http.post("https://relay4.staging.oblivious.network/.oblivious-network/echo", body, {
     headers: {
       "Content-Type": "message/ohttp-chunked-req",
     },
   });
   check(res, {
     "status is 200": (r) => r.status === 200,
-    "protocol is HTTP/2": (r) => r.proto === "HTTP/2.0",
+    // "protocol is HTTP/2": (r) => r.proto === "HTTP/2.0",
   });
 }
